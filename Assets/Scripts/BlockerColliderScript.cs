514052a41b12a263ad479ea098a41d47bf735fec
Agvargas <sagrav951@yahoo.com>
2020-08-20 20:37:15 -0500

    Updating UI images and interactions...

---
+++ Assets/Scripts/BlockerColliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerColliderScript : MonoBehaviour
+{
+    public GameObject popUp;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        popUp.SetActive(true);
+    //    }
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+            popUp.SetActive(true);
+
+    }
+}
