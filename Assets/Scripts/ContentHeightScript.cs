e0559029d49caf24a8eb95800e73836bd9e9ddae
Agvargas <sagrav951@yahoo.com>
2020-08-22 00:02:20 -0500

    Adding room 5 images gallery programing... And local DB accesing...
    se añade programacion para llamar imagenes en la galeria de 5 años, y script para lectura y almacenamiento local de la BD

--- Assets/Scripts/ContentHeightScript.cs
+++ Assets/Scripts/ContentHeightScript.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ContentHeightScript : MonoBehaviour
 {
+    public GameObject imagePrefab;
+
     public float height;
     private float childs;
+    
+    public GameController data;
+
+
+    public string year;
+
+    private void Awake()
+    {
+        for (int i = 0; i < 1309; i++)
+        {
+            if (data.dataPlayers[i][4] == year)
+            {
+                GameObject photo= Instantiate(imagePrefab, transform);
+                photo.name = data.dataPlayers[i][0];
+                photo.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/" + year + "/" + data.dataPlayers[i][0]);
+            }
+        }
+    }
 
     void Start()
     {
@@ -15,5 +36,10 @@ public class ContentHeightScript : MonoBehaviour
         height += 10;
 
         GetComponent<RectTransform>().sizeDelta = new Vector2(0, height);
+    }
+
+    public void Year(string a)
+    {
+        year = a;
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class GameController : MonoBehaviour
+{
+    public string[][] dataPlayers = new string[1309][];
+    private string[] dataList = new string[1309];
+
+    void Start()
+    {
+        dataList = File.ReadAllLines(Application.streamingAssetsPath + "/BDCondecorados.txt", Encoding.UTF7);
+        
+        for (int i = 0; i < dataPlayers.Length; i++)
+        {
+            dataPlayers[i] = dataList[i].Split(';');
+        }
+
+        //Instantiate(Resources.Load<GameObject>("Sprites/5/Adriana Grajales Ochoa"));
+        //Resources.Load<Sprite>("Sprites/5/Adriana Sofia Parra Marin");
+    }
+}
+
+
