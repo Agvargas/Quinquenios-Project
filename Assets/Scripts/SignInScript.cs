1d36654a27cf58d0ae048c95e17123783f19efe4
Agvargas <sagrav951@yahoo.com>
2020-09-22 17:12:31 -0500

    Updating UI elements and functionality...

--- Assets/Scripts/SignInScript.cs
+++ Assets/Scripts/SignInScript.cs
@@ -12,30 +12,64 @@ public class SignInScript : MonoBehaviour
     public Toggle acceptCheck;
     public GameController data;
     public BoxCollider blockerCollider;
+    public GameObject rightImage;
+    public GameObject wrongImage;
 
     public void CheckInfo()
     {
-        print(data.dataPlayers.Length);
+        
         for (int i = 0; i < data.dataPlayers.Length; i++)
         {
             if (data.dataPlayers[i][0] == nameInput.text)
             {
+                print("ok1");
                 if (data.dataPlayers[i][1] == idInput.text)
                 {
+                    print("ok2");
                     if (data.dataPlayers[i][2] == bussinesInput.text)
                     {
+                        print("ok3");
                         if (data.dataPlayers[i][3] == cityInput.text)
                         {
+                            print("ok4");
                             if (acceptCheck.isOn)
                             {
+                                print("ok5");
+                                wrongImage.SetActive(false);
                                 data.userID = data.dataPlayers[i][5];
                                 //data.userName = data.dataPlayers[i][0];
                                 blockerCollider.enabled = false;
-                                gameObject.SetActive(false);
+                                rightImage.SetActive(true);
+                                i = data.dataPlayers.Length;
+                            }
+                            else
+                            {
+                                print("wrong5");
+                                wrongImage.SetActive(true);
                             }
                         }
+                        else
+                        {
+                            print("wrong4");
+                            wrongImage.SetActive(true);
+                        }
+                    }
+                    else
+                    {
+                        print("wrong3");
+                        wrongImage.SetActive(true);
                     }
                 }
+                else
+                {
+                    print("wrong2");
+                    wrongImage.SetActive(true);
+                }
+            }
+            else
+            {
+                print("wrong1");
+                wrongImage.SetActive(true);
             }
         }
     }
